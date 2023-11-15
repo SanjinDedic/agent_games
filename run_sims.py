ee114aaaef5de93225e7f151050c2040a6513481
Sanjin84 <ozrobotix@gmail.com>
2023-11-15 21:43:19 +1100

    naming change on sims so that sims2 is now 2 parameters

--- run_sims.py
+++ run_sims.py
@@ -1,6 +1,5 @@
 import argparse
 from multiple_players_game import run_simulation_many_times
-from multiple_players_game import run_animation
 
 def main():
     # Create the parser
@@ -8,18 +7,16 @@ def main():
 
     # Add arguments
     parser.add_argument('-sims', type=int, required=True, help='Number of simulations to run')
-    parser.add_argument('-refresh', type=int, required=True, help='refresh rate for animation')
-    # Parse arguments
     args = parser.parse_args()
 
-    run_animation(args.refresh, args.sims)
+
 
     # Run the simulation
-    #results = run_simulation_many_times(args.sims)
+    results = run_simulation_many_times(args.sims)
 
     # Print results
-    #print("Simulation Results:")
-    #print(results)
+    print("Simulation Results:")
+    print(results)
 
 if __name__ == "__main__":
     main()
