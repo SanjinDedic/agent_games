f0414b079b8f24c5430f23f43a8d904f2a76e85d
ubuntu <ubuntu@API.myguest.virtualbox.org>
2023-11-19 15:18:05 +1100

    improved validation2

--- test_classes/BankRoll3.py
+++ test_classes/BankRoll3.py
@@ -3,6 +3,6 @@ from player_base import Player
 class CustomPlayer(Player):
 
     def make_decision(self, game_state):
-        if game_state["roll_no"] > 2:
+        if game_state["roll_no"] == 3:
             return 'bank'
         return 'continue'
