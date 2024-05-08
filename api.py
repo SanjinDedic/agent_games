999bd9e7b8a508c45c8c8b0fb357db2e3f3fd8a8
Sanjin Dedic <sanjindedic@Sanjins-Laptop.local>
2024-05-08 20:52:10 +1000

    testing deploy.yml

--- api.py
+++ api.py
@@ -44,7 +44,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Success, server is up and running (deploy.yml works2)"}
+    return {"message": "Success, server is up and running (deploy.yml works3)"}
 
 
 @app.post("/agent_login")
