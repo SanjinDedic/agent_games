9df5426947716b68b85dd475265575155b53ccac
SanjinDedic <ozrobotix@gmail.com>
2024-06-20 10:42:00 +1000

    test speed improved for api sim and test team

--- tests/test_api_sim.py
+++ tests/test_api_sim.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import pytest
-import time
 from fastapi.testclient import TestClient
 from sqlmodel import Session, select
 
@@ -13,20 +12,18 @@ from models_db import League, SimulationResult
 from tests.database_setup import setup_test_db
 os.environ["TESTING"] = "1"
 
-ADMIN_VALID_TOKEN = ""
-
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def setup_database():
     setup_test_db()
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def db_session():
     engine = get_db_engine()
     with Session(engine) as session:
         yield session
         session.rollback()
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def client(db_session):
     def get_db_session_override():
         return db_session
@@ -36,52 +33,45 @@ def client(db_session):
     yield client
     app.dependency_overrides.clear()
 
-def test_admin_login(client: TestClient):
-    global ADMIN_TOKEN
-    # Get the token for the admin
+@pytest.fixture(scope="module")
+def admin_token(client):
     admin_login_response = client.post("/admin_login", json={"username": "Administrator", "password": "BOSSMAN"})
     assert admin_login_response.status_code == 200
-    print("This is the admin login response", admin_login_response.json())
-    ADMIN_TOKEN = admin_login_response.json()["data"]["access_token"]
-    assert ADMIN_TOKEN != ""
+    return admin_login_response.json()["data"]["access_token"]
 
-def test_run_simulation(client: TestClient, db_session: Session):
-    global ADMIN_TOKEN
-    print("ADMIN_TOKEN", ADMIN_TOKEN, "simulation called")
+@pytest.fixture(scope="module")
+def non_admin_token(client):
+    non_admin_token = client.post("/team_login", json={"name": "BrunswickSC1", "password": "ighEMkOP"}).json()["data"]["access_token"]
+    return non_admin_token
+
+def test_run_simulation(client, db_session, admin_token):
     simulation_response = client.post(
         "/run_simulation",
         json={"league_name": "comp_test", "num_simulations": 100},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert simulation_response.status_code == 200
-    print("This is the simulation response", simulation_response.json())
     assert "total_points" in simulation_response.json()["data"]
 
-    # Check if the simulation results are logged in the database
     simulation_results = db_session.exec(select(SimulationResult)).all()
     assert len(simulation_results) > 0
 
-def test_get_all_league_results(client: TestClient):
-    global ADMIN_TOKEN
+def test_get_all_league_results(client, admin_token, non_admin_token):
     simulation_response = client.post(
         "/run_simulation",
         json={"league_name": "comp_test", "num_simulations": 10},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert simulation_response.status_code == 200
+    
     league_results_response = client.post(
         "/get_all_league_results",
         json={"name": "comp_test"},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
-)
-    print("Version 1")
-    print(league_results_response.json())
+        headers={"Authorization": f"Bearer {admin_token}"}
+    )
     assert league_results_response.status_code == 200
     assert isinstance(league_results_response.json()["data"]["all_results"], list)
 
-
-    # Test with non-admin user
-    non_admin_token = client.post("/team_login", json={"name": "BrunswickSC1", "password": "ighEMkOP"}).json()["data"]["access_token"]
     unauthorized_response = client.post(
         "/get_all_league_results",
         json={"name": "comp_test"},
@@ -89,47 +79,39 @@ def test_get_all_league_results(client: TestClient):
     )
     assert unauthorized_response.json()["message"] == "Only admin users can view league results"
 
-def test_publish_results(client: TestClient, db_session: Session):
-    global ADMIN_TOKEN
-    # Run a simulation
+def test_publish_results(client, db_session, admin_token, non_admin_token):
     simulation_response = client.post(
         "/run_simulation",
         json={"league_name": "comp_test", "num_simulations": 10},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert simulation_response.status_code == 200
     simulation_id = simulation_response.json()["data"]["simulation_id"]
 
-    # Publish the simulation results
     publish_response = client.post(
         "/publish_results",
         json={"league_name": "comp_test", "id": simulation_id},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert publish_response.status_code == 200
     assert publish_response.json()["status"] == "success"
     assert publish_response.json()["message"] == "Simulation results for league 'comp_test' published successfully"
 
-    # Check if the simulation results are marked as published in the database
     simulation_result = db_session.exec(select(SimulationResult).where(SimulationResult.id == simulation_id)).one()
     assert simulation_result.published == True
 
-    # Try publishing the same results again as an admin
     publish_again_response = client.post(
         "/publish_results",
         json={"league_name": "comp_test", "id": simulation_id},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert publish_again_response.status_code == 200
     assert publish_again_response.json()["status"] == "success"
     assert publish_again_response.json()["message"] == "Simulation results for league 'comp_test' published successfully"
 
-    # Check if the simulation results are still marked as published in the database
     simulation_result = db_session.exec(select(SimulationResult).where(SimulationResult.id == simulation_id)).one()
     assert simulation_result.published == True
 
-    # Test with non-admin user
-    non_admin_token = client.post("/team_login", json={"name": "BrunswickSC1", "password": "ighEMkOP"}).json()["data"]["access_token"]
     unauthorized_response = client.post(
         "/publish_results",
         json={"league_name": "comp_test", "id": simulation_id},
@@ -137,87 +119,73 @@ def test_publish_results(client: TestClient, db_session: Session):
     )
     assert unauthorized_response.json()["message"] == "Only admin users can publish league results"
 
-    # Check if the simulation results are still marked as published in the database
     simulation_result = db_session.exec(select(SimulationResult).where(SimulationResult.id == simulation_id)).one()
     assert simulation_result.published == True
 
-
-def test_publish_one_simulation_per_league(client: TestClient, db_session: Session):
-    global ADMIN_TOKEN
-    # Run the first simulation
+def test_publish_one_simulation_per_league(client, db_session, admin_token):
     simulation_response1 = client.post(
         "/run_simulation",
         json={"league_name": "comp_test", "num_simulations": 10},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert simulation_response1.status_code == 200
     simulation_id1 = simulation_response1.json()["data"]["simulation_id"]
 
-    # Publish the first simulation results
     publish_response1 = client.post(
         "/publish_results",
         json={"league_name": "comp_test", "id": simulation_id1},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert publish_response1.status_code == 200
     assert publish_response1.json()["status"] == "success"
     assert publish_response1.json()["message"] == "Simulation results for league 'comp_test' published successfully"
 
-    # Run the second simulation
     simulation_response2 = client.post(
         "/run_simulation",
         json={"league_name": "comp_test", "num_simulations": 10},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert simulation_response2.status_code == 200
     simulation_id2 = simulation_response2.json()["data"]["simulation_id"]
 
-    # Try to publish the second simulation results
     publish_response2 = client.post(
         "/publish_results",
         json={"league_name": "comp_test", "id": simulation_id2},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert publish_response2.status_code == 200
     assert publish_response2.json()["status"] == "success"
 
-    # Check if the first simulation results are still marked as published in the database
     simulation_result1 = db_session.exec(select(SimulationResult).where(SimulationResult.id == simulation_id1)).one()
     assert simulation_result1.published == False
 
-    # Check if the second simulation results are not marked as published in the database
     simulation_result2 = db_session.exec(select(SimulationResult).where(SimulationResult.id == simulation_id2)).one()
     assert simulation_result2.published == True
 
-def test_get_published_results_for_league(client: TestClient, db_session: Session):
-    global ADMIN_TOKEN
-    # Run a simulation
+def test_get_published_results_for_league(client, db_session, admin_token):
     simulation_response = client.post(
         "/run_simulation",
         json={"league_name": "comp_test", "num_simulations": 10},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert simulation_response.status_code == 200
     simulation_id = simulation_response.json()["data"]["simulation_id"]
 
-    # Publish the simulation results
     publish_response = client.post(
         "/publish_results",
         json={"league_name": "comp_test", "id": simulation_id},
-        headers={"Authorization": f"Bearer {ADMIN_TOKEN}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert publish_response.status_code == 200
     assert publish_response.json()["status"] == "success"
     assert publish_response.json()["message"] == "Simulation results for league 'comp_test' published successfully"
 
-    # Get the published results for the league
     get_published_response = client.post(
         "/get_published_results_for_league",
         json={"name": "comp_test"}
     )
     assert get_published_response.status_code == 200
     published_result = get_published_response.json()["data"]
-    print(published_result)
-    # Check if the simulation results are marked as published in the database
+
     simulation_result = db_session.exec(select(SimulationResult).where(SimulationResult.id == simulation_id)).one()
     assert simulation_result.published == True
--- tests/test_team.py
+++ tests/test_team.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import pytest
-import time
 from fastapi.testclient import TestClient
 from sqlmodel import Session, select
 
@@ -13,21 +12,18 @@ from models_db import Team
 from tests.database_setup import setup_test_db
 os.environ["TESTING"] = "1"
 
-ADMIN_VALID_TOKEN = ""
-TEAM_TOKEN = ""
-
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def setup_database():
     setup_test_db()
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def db_session():
     engine = get_db_engine()
     with Session(engine) as session:
         yield session
         session.rollback()
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def client(db_session):
     def get_db_session_override():
         return db_session
@@ -37,15 +33,21 @@ def client(db_session):
     yield client
     app.dependency_overrides.clear()
 
-def test_get_token(client: TestClient):
-    global ADMIN_VALID_TOKEN
-
+@pytest.fixture(scope="module")
+def admin_token(client):
     login_response = client.post("/admin_login", json={"username": "Administrator", "password": "BOSSMAN"})
     assert login_response.status_code == 200
     token = login_response.json()["data"]["access_token"]
-    ADMIN_VALID_TOKEN = token
+    return token
 
-def test_team_login(client: TestClient):
+@pytest.fixture(scope="module")
+def team_token(client):
+    response = client.post("/team_login", json={"name": "BrunswickSC1", "password": "ighEMkOP"})
+    assert response.status_code == 200
+    token = response.json()["data"]["access_token"]
+    return token
+
+def test_team_login(client):
     response = client.post("/team_login", json={"name": "BrunswickSC1", "password": "ighEMkOP"})
     assert response.status_code == 200
     assert "access_token" in response.json()["data"]
@@ -62,84 +64,61 @@ def test_team_login(client: TestClient):
     response = client.post("/team_login", json={"name": " ", "password": "ighEMkOP"})
     assert response.status_code == 422
 
-## Review the test (sending admin token instead of team token on team_login)
-def test_league_assign(client: TestClient, db_session):
-    global TEAM_TOKEN, ADMIN_VALID_TOKEN
-    # Get the token for the team
-    team_name = "BrunswickSC1"
-    team_login_response = client.post(
-        "/team_login", json={"name": f"{team_name}", "password": "ighEMkOP"}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
-    assert team_login_response.status_code == 200
-    TEAM_TOKEN = team_login_response.json()["data"]["access_token"]
-    print("Team Token:", TEAM_TOKEN)
+def test_league_assign(client, team_token):
     league_name = "unassigned"
-    # Assign the team to the league
+    team_name = "BrunswickSC1"
     response = client.post(
         "/league_assign",
-        json={"name":f"{league_name}"},
-        headers={"Authorization": f"Bearer {TEAM_TOKEN}"})
-    print("League Assign Response:", response.json())
+        json={"name": league_name},
+        headers={"Authorization": f"Bearer {team_token}"}
+    )
     assert response.status_code == 200
-    assert response.json() == {"status":"success", "message": f"Team '{team_name}' assigned to league '{league_name}'", "data": None}
+    assert response.json() == {"status": "success", "message": f"Team '{team_name}' assigned to league '{league_name}'", "data": None}
 
-
-
-
-def test_team_create(client: TestClient, db_session):
-    global ADMIN_VALID_TOKEN
-    # Create a team
+def test_team_create(client, db_session, admin_token):
     team_name = "new_test_team"
     team_password = "new_test_password"
     team_school = "new_test_school"
-    response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
     assert "access_token" in response.json()["data"]
     assert response.json()["data"]["token_type"] == "bearer"
 
-    # Check if the team is created in the database
     team = db_session.exec(select(Team).where(Team.name == team_name)).one_or_none()
     assert team is not None
     assert team.name == team_name
     assert team.school_name == team_school
     assert team.verify_password(team_password)
 
-    # Try to create a team with the same name
-    response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
     assert response.json()["status"] == "failed"
 
-    # Try to create a team with missing fields
-    response = client.post("/team_create", json={"name": "missing_fields_team"}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/team_create", json={"name": "missing_fields_team"}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 422
 
-    response = client.post("/team_create", json={"name": "missing_fields_team", "school": team_school}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/team_create", json={"name": "missing_fields_team", "school": team_school}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 422
 
-    response = client.post("/team_create", json={"password": team_password, "school": team_school}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/team_create", json={"password": team_password, "school": team_school}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 422
 
-    # Try to create a team with no authorization
     response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school})
     assert response.status_code == 401
 
-def test_delete_team(client: TestClient):
-    global ADMIN_VALID_TOKEN
-    # Create a team
+def test_delete_team(client, admin_token):
     team_name = "test_team"
     team_password = "test_password"
     team_school = "test_school"
-    response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/team_create", json={"name": team_name, "password": team_password, "school_name": team_school}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
 
-    # Delete the team
-    response = client.post("/delete_team", json={"name": team_name}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
-    print("Delete Team Response:", response.json())
+    response = client.post("/delete_team", json={"name": team_name}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
     assert response.json()["status"] == "success"
     assert response.json()["message"] == f"Team '{team_name}' deleted successfully"
 
-    # Try to delete a non-existent team
-    response = client.post("/delete_team", json={"name": "non_existent_team"}, headers={"Authorization": f"Bearer {ADMIN_VALID_TOKEN}"})
+    response = client.post("/delete_team", json={"name": "non_existent_team"}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
     assert response.json()["status"] == "failed"
     assert response.json()["message"] == "Team 'non_existent_team' not found"
