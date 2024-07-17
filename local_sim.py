8b964efbc0e5103cdcf9ce9828ddad08c9ae0578
SanjinDedic <ozrobotix@gmail.com>
2024-07-18 07:52:43 +1000

    local sim updated and local sim via api script added

--- local_sim.py
+++ local_sim.py
@@ -1,21 +1,18 @@
 import os
 import sys
-from games.greedy_pig.greedy_pig import Game
-from games.greedy_pig.greedy_pig_sim import animate_simulations, run_simulations
+from games.greedy_pig.greedy_pig import GreedyPigGame, run_simulations
 from models_db import League
 
-
-
+# Add the project root directory to the Python path
 current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+sys.path.append(project_root)
+
 test_league_folder = os.path.join(current_dir, "games", "greedy_pig", "leagues", "test_league")
 
-test_league2 = League(folder=test_league_folder, name="Test League")
+test_league = League(folder=test_league_folder, name="Test League", game="greedy_pig")
 
-game2 = Game(test_league2)
-print("players in local sim game2",game2.players)
-
-# Run 100 simulations
-results = run_simulations(100, test_league2)
-
-print("Results of 100 simulations:")
+# Run 1000 simulations
+num_simulations = 1000
+results = run_simulations(num_simulations, test_league)
 print(results)
---
+++ local_sim_api.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from fastapi.testclient import TestClient
+from sqlmodel import Session, create_engine
+
+# Add the project root directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+sys.path.append(project_root)
+
+from api import app
+from config import ROOT_DIR
+
+# Connect to local copy of prod database
+#If you have errors run python3 production_database_setup.py
+db_path = os.path.join(ROOT_DIR, "teams.db")
+engine = create_engine(f"sqlite:///{db_path}")
+
+def get_db_session():
+    with Session(engine) as session:
+        yield session
+
+app.dependency_overrides[get_db_session] = get_db_session
+
+client = TestClient(app)
+
+def get_admin_token():
+    response = client.post("/admin_login", json={"username": "Administrator", "password": "BOSSMAN"})
+    return response.json()["data"]["access_token"]
+
+def run_simulation(league_name, num_simulations, token):
+    response = client.post(
+        "/run_simulation",
+        json={"league_name": league_name, "num_simulations": num_simulations},
+        headers={"Authorization": f"Bearer {token}"}
+    )
+    return response.json()
+
+if __name__ == "__main__":
+    admin_token = get_admin_token()
+    league_name = "week1"
+    num_simulations = 1000
+
+    result = run_simulation(league_name, num_simulations, admin_token)
+    print(result)
